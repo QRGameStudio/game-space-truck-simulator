aa3534b2febf9fba3d8d22fd4f4de25edfaa0236
Adam Hlaváček <git@adamhlavacek.com>
2024-06-02 16:08:08 +0200

    feat: style map

--- game.js
+++ game.js
@@ -357,10 +357,6 @@ async function start() {
                 map: {
                     x: Math.floor((obj.x - minX) / mapPrecision) + 100,
                     y: Math.floor((obj.y - minY) / mapPrecision) + 100,
-                    max: {
-                        x: Math.floor((maxX - minX) / mapPrecision) + 200,
-                        y: Math.floor((maxY - minY) / mapPrecision) + 200
-                    }
                 },
                 isPlayer: obj.t === PLAYER.t,
                 name: obj?.name,
@@ -380,19 +376,59 @@ async function start() {
         }
 
         MODAL.show('targetSelection', {
-            fields: mapFields
+            fields: mapFields,
+            map: {
+                max: {
+                    x: Math.floor((maxX - minX) / mapPrecision) + 200,
+                    y: Math.floor((maxY - minY) / mapPrecision) + 200
+                }
+            }
         }, functions).then();
         setTimeout(() => {
             const mapContainer = $('.system-map-container');
+
+            // Scroll map to the player position
+            const mapPlayer = $('.system-player');
             const containerRect = mapContainer.getBoundingClientRect();
-            const fieldPlayer = mapFields.find(x => x.isPlayer);
-            const verticalOffset = fieldPlayer.map.y - containerRect.top + mapContainer.scrollTop;
-            const horizontalOffset = fieldPlayer.map.x - containerRect.left + mapContainer.scrollLeft;
-            const verticalScrollTo = verticalOffset - (mapContainer.clientHeight / 2);
-            const horizontalScrollTo = horizontalOffset - (mapContainer.clientWidth / 2);
+            const playerRect = mapPlayer.getBoundingClientRect();
+            const verticalOffset = playerRect.top - containerRect.top + mapContainer.scrollTop;
+            const horizontalOffset = playerRect.left - containerRect.left + mapContainer.scrollLeft;
+            const verticalScrollTo = verticalOffset - (mapContainer.clientHeight / 2) + (mapPlayer.clientHeight / 2);
+            const horizontalScrollTo = horizontalOffset - (mapContainer.clientWidth / 2) + (mapPlayer.clientWidth / 2);
             mapContainer.scrollTop = verticalScrollTo;
             mapContainer.scrollLeft = horizontalScrollTo;
-            console.log('Scrolling to', horizontalScrollTo, verticalScrollTo, fieldPlayer);
+
+            // Move overlapping elements
+            function checkOverlap(element1, element2) {
+                const rect1 = element1.getBoundingClientRect();
+                const rect2 = element2.getBoundingClientRect();
+
+                return !(rect1.right < rect2.left ||
+                    rect1.left > rect2.right ||
+                    rect1.bottom < rect2.top ||
+                    rect1.top > rect2.bottom);
+            }
+
+            function moveRight(element, referenceElement) {
+                const referenceRect = referenceElement.getBoundingClientRect();
+                const currentLeft = parseInt(window.getComputedStyle(element).left, 10);
+                element.style.left = `${currentLeft + referenceRect.width + 10}px`; // Move right by the width of referenceElement + some margin
+            }
+
+            const children = document.querySelectorAll('.system-map > .system');
+            let moved;
+
+            do {
+                moved = false;
+                for (let i = 0; i < children.length; i++) {
+                    for (let j = i + 1; j < children.length; j++) {
+                        if (checkOverlap(children[i], children[j])) {
+                            moveRight(children[j], children[i]);
+                            moved = true;
+                        }
+                    }
+                }
+            } while (moved);
         }, 100);
     }
 
